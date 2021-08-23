0741bbe07138583193be83f5797174d8e8bddabd
Gleywson <gleywson@gmail.com>
2021-08-22 22:12:01 -0300

    Criado o mecanismo de menu. Falta apenas implementar seguindo o modelo

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -19,20 +19,18 @@ public class Main {
 
             opcao = menu.nextInt();
 
-            if (opcao == 4) {
+            if (opcao == 0) {
                 System.out.print("\nAté logo!");
                 menu.close();
             }
 
             switch (opcao) {
                 case 1:
-                    System.out.print("\nCADASTRO DE PRODUTOS");
+                    produto();
                     break;
-
                 case 2:
                     System.out.print("\nMOVIMENTAÇÃO\n");
                     break;
-
                 case 3:
                     System.out.print("\nREAUSTE DE PREÇOS\n");
                     break;
@@ -42,7 +40,44 @@ public class Main {
                     System.out.print("\nOpção Inválida!");
                     break;
             }
-        } while (opcao != 4);
+        } while (opcao != 0);
 
+    }
+
+    public static void produto() {
+        Scanner input = new Scanner(System.in);
+        int opcao = 0;
+        do {
+            System.out.print("##---CADASTRO DE PRODUTOS----##\n");
+            System.out.print("|-----------------------------|\n");
+            System.out.print("| 1 - INCLUSAO    |\n");
+            System.out.print("| 2 - ALTERACAO   |\n");
+            System.out.print("| 3 - CONSULTA    |\n");
+            System.out.print("| 4 - EXCLUSAO    |\n");
+            System.out.print("| 0 - RETORNAR    |\n");
+            System.out.print("|-----------------------------|\n");
+            System.out.print("Digite uma opção: ");
+
+            opcao = input.nextInt();
+
+            switch (opcao) {
+                case 1:
+                    System.out.println("Inclusão de produto");
+                    break;
+                case 2:
+                    System.out.println("Alteração de Produto");
+                    break;
+                case 3:
+                    System.out.println("Exclusão de produtos");
+                    break;
+                case 4:
+                    System.out.println("Relatório de produtos");
+                    break;
+                case 0:
+                    break;
+                default:
+                    System.out.println("Opção inválida");
+            }
+        } while (opcao != 0);
     }
 }
